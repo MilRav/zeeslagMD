ef1f09ec86a8f41ca5d00653cc769838963e76ce
Krispijn <322401+krispijn@users.noreply.github.com>
2023-06-02 17:07:19 +0200

    Added recording of scores and a way to show hostoric stats in console.

--- scripts/score.js
+++ scripts/score.js
@@ -35,6 +35,7 @@ function checkScore() {
 
 //Open the correct page's when the player wins or loses
 function win() {
+    recordScore()
     infoDisplay.textContent = 'You sunk all the computers ships. YOU WON!';
     gameStats.gameOver = true;
     if (gameStats.player.hitPercentage >= DUCKY_THRESHOLD) {
@@ -44,9 +45,53 @@ function win() {
     }
 }
 function lose() {
+    recordScore()
     infoDisplay.textContent = 'The computer has sunk all your ships. YOU LOST!';
     gameStats.gameOver = true;
     window.open("../pages/gameover.html", "_self");
 }
 
+function recordScore(){
+    let result = (gameStats.computer.shipsSunk.length === 5) ? 'win' : 'loss'
+    // retreive score object from local storage
+    let _oGameHistoryFromStorage = JSON.parse(localStorage.getItem('gamehistory'));
+    // append this game
+    if (!_oGameHistoryFromStorage){
+        _oGameHistoryFromStorage = { scores: []}
+    }
 
+    _oGameHistoryFromStorage.scores.push({'time': Date.now(),
+        'result': result, 
+        'playerScore': gameStats.player.hitPercentage})
+
+    localStorage.setItem('gamehistory', JSON.stringify(_oGameHistoryFromStorage))
+
+}
+
+// shows historic game statistics in the console
+function gameStatistics(){
+    let _oStats = {
+        'total games enjoyed': 0,
+        'wins': 0,
+        'losses': 0,
+        'highest score': 0,
+        'average score': 0
+    }
+
+    let _oGameHistoryFromStorage = JSON.parse(localStorage.getItem('gamehistory'))
+    _oGameHistoryFromStorage.scores.forEach((game) => {
+        if (game.playerScore > _oStats["highest score"]) _oStats["highest score"] = game.playerScore
+        _oStats["average score"] += game.playerScore
+        game.result == 'win' ? _oStats["wins"]++ :  _oStats["losses"]++ 
+    })
+
+    _oStats["average score"] =  _oStats["average score"] /  _oGameHistoryFromStorage.scores.length
+    _oStats["total games enjoyed"] = _oGameHistoryFromStorage.scores.length
+
+    return _oStats
+}
+
+// resets historic stats
+function resetStatistics(){
+    localStorage.setItem('gamehistory', JSON.stringify( { scores: []} ))
+}
