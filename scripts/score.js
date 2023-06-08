0c88b32e2d3978b71dff556a40497607f73fcbfd
Krispijn Scholte <322401+krispijn@users.noreply.github.com>
2023-06-08 15:50:45 +0200

    Changed to rolling statistics instead of complete history.

--- scripts/score.js
+++ scripts/score.js
@@ -52,71 +52,58 @@ function lose() {
 
 // appends the score of the game to local storage
 function recordScore(){
-    let result = (gameStats.computer.shipsSunk.length === 5) ? 'win' : 'loss'
-    let _oGameHistoryFromStorage = JSON.parse(localStorage.getItem('gamehistory'));
+    let _oGameStats = JSON.parse(localStorage.getItem('gamestats'));
 
-    if (!_oGameHistoryFromStorage){
-        _oGameHistoryFromStorage = { scores: []}
+    if (!_oGameStats){
+        _oGameStats = {
+            'total games enjoyed': 0,
+            'wins': 0,
+            'losses': 0,
+            'highest score': 0,
+            'average score': 0,
+            'average game time': 0,
+            'longest game time': 0,
+            'shortest game time': 36000, // 10 hours :)
+            'average no. rounds': 0,
+            'most rounds': 0,
+            'least rounds': 100 // maximum possible rounds for 10x10
+        }
     }
 
-    _oGameHistoryFromStorage.scores.push({
-        'time': Date.now(),
-        'rounds': gameStats.round,
-        'duration': (Date.now() - gameStats.startTime)/1000, //in seconds
-        'result': result, 
-        'playerScore': gameStats.player.hitPercentage
-        
-    })
+    let _nGames = ++_oGameStats['total games enjoyed']
+    let _nDuration = (Date.now() - gameStats.startTime)/1000 //in seconds
+    let _nPlayerScore = gameStats.player.hitPercentage
+    let _nRounds = gameStats.round
 
-    localStorage.setItem('gamehistory', JSON.stringify(_oGameHistoryFromStorage))
+    let _bResult = (gameStats.computer.shipsSunk.length === 5) ? ++_oGameStats['wins'] : ++_oGameStats['losses']
+
+    // score
+    if (_nPlayerScore > _oGameStats["highest score"]) _oGameStats["highest score"] = _nPlayerScore
+
+    // time
+    if (_nDuration > _oGameStats["longest game time"]) _oGameStats["longest game time"] = _nDuration
+    if (_nDuration < _oGameStats["shortest game time"]) _oGameStats["shortest game time"] = _nDuration
+
+    // rounds
+    if (_nRounds > _oGameStats["most rounds"]) _oGameStats["most rounds"] = _nRounds
+    if (_nRounds < _oGameStats["least rounds"]) _oGameStats["least rounds"] = _nRounds
+
+    // recalculate averages
+    _oGameStats["average score"] =  ( (_oGameStats["average score"] * (_nGames-1)) + _nPlayerScore) /  _nGames
+    _oGameStats["average game time"] =  ( (_oGameStats["average game time"] * (_nGames-1)) + _nDuration) /  _nGames
+    _oGameStats["average no. rounds"] = ( (_oGameStats["average no. rounds"] * (_nGames-1)) + _nRounds) /  _nGames
+
+    localStorage.setItem('gamestats', JSON.stringify(_oGameStats))
 
 }
 
 // shows historic game statistics in the console
 function gameStatistics(){
-    let _oStats = {
-        'total games enjoyed': 0,
-        'wins': 0,
-        'losses': 0,
-        'highest score': 0,
-        'average score': 0,
-        'average game time': 0,
-        'longest game time': 0,
-        'shortest game time': 36000, // 10 hours :)
-        'average no. rounds': 0,
-        'most rounds': 0,
-        'least rounds': 100 // maximum possible rounds for 10x10
-    }
-
-    let _oGameHistoryFromStorage = JSON.parse(localStorage.getItem('gamehistory'))
-    _oGameHistoryFromStorage.scores.forEach((game) => {
-        // score
-        if (game.playerScore > _oStats["highest score"]) _oStats["highest score"] = game.playerScore
-        _oStats["average score"] += game.playerScore
-
-        // time
-        if (game.playerScore > _oStats["longest game time"]) _oStats["longest game time"] = game.duration
-        if (game.playerScore < _oStats["shortest game time"]) _oStats["shortest game time"] = game.duration
-        _oStats["average game time"] += game.duration
-
-        // rounds
-        if (game.playerScore > _oStats["most rounds"]) _oStats["most rounds"] = game.rounds
-        if (game.playerScore < _oStats["least rounds"]) _oStats["least rounds"] = game.rounds
-        _oStats["average no. rounds"] += game.rounds
-        
-        game.result == 'win' ? _oStats["wins"]++ :  _oStats["losses"]++ 
-    })
-
-    _oStats["average score"] =  _oStats["average score"] /  _oGameHistoryFromStorage.scores.length
-    _oStats["average game time"] =  _oStats["average game time"] /  _oGameHistoryFromStorage.scores.length
-    _oStats["average no. rounds"] = _oStats["average no. rounds"] /  _oGameHistoryFromStorage.scores.length
-
-    _oStats["total games enjoyed"] = _oGameHistoryFromStorage.scores.length
-
+    let _oStats = JSON.parse(localStorage.getItem('gamestats'))
     return _oStats
 }
 
 // resets historic stats
 function resetStatistics(){
-    localStorage.setItem('gamehistory', JSON.stringify( { scores: []} ))
+    localStorage.removeItem('gamestats')
 }
